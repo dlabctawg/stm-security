75c29f3c6ce1bfb33e7e4d3ad0b88450972c2934
Brooks Ambrose <brooksambrose@gmail.com>
2016-09-08 18:44:48 -0700

    timing2

--- stmrun.R
+++ stmrun.R
@@ -11,6 +11,9 @@ load('meta-security.RData')
 
 # run provisional topic model testing that documents and metadata are properly sorted
 ## note that no covariates are used but content and prevalance terms could be easily added
+t0<-proc.time()
 if(identical(names(bow2stm$documents),meta$speechID)) {
-	system.time(mod<-stm(bow2stm$documents,bow2stm$vocab,K=10,data=meta))
+	mod<-stm(bow2stm$documents,bow2stm$vocab,K=10,data=meta)
 }
+t1<-proc.time()
+cat('STM model fit in',round((t1-t0)/60,2)[3],'minutes.')
