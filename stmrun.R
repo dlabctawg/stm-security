f2a43b898efdcdfa184170ffaacef4876f460148
Brooks Ambrose <brooksambrose@gmail.com>
2016-09-08 17:17:22 -0700

    Good to go!
    See stmrun.R for usage

---
+++ stmrun.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+cat('\014')
+library(stm)
+require(data.table)
+
+# Load stm-formatted bag of words
+load('bow2stm-security.RData')
+
+# Load metadata
+load('meta-security.RData')
+
+# run provisional topic model testing that documents and metadata are properly sorted
+## note that no covariates are used but content and prevalance terms could be easily added
+if(identical(names(bow2stm$documents),meta$speechID)) {
+	mod<-stm(bow2stm$documents,bow2stm$vocab,K=10,data=meta)
+}
