c42bd214f6501bd988827647f8bf80467a4b1458
Brooks Ambrose <brooksambrose@gmail.com>
2016-09-14 10:24:56 -0700

    tidy

--- stmrun-contprev.R
+++ stmrun-contprev.R
@@ -11,7 +11,7 @@ load('meta-security-time.RData')
 
 # recode
 ## party
-summary(meta)
+meta[,.N,by=party]
 setkey(meta,party)
 meta['R',party:='Republican']
 setkey(meta,party)
@@ -37,7 +37,7 @@ setkey(meta,speechID)
 # run provisional topic model testing that documents and metadata are properly sorted
 ## note that no covariates are used but content and prevalance terms could be easily added
 t0<-proc.time()
-cat(c(rep('#',10),' Fitting content = ~after911 ',rep('#',10)),sep='')
+cat(c(rep('#',10),' Fitting content = ~after911 ',rep('#',10),'\n\n'),sep='')
 if(identical(names(bow2stm$documents),meta$speechID)) {
 	mod<-stm(bow2stm$documents,bow2stm$vocab,K=10,data=meta,content = ~after911)
 }
@@ -48,7 +48,7 @@ save(mod,file='stm-model-cont-911.RData')
 # run provisional topic model testing that documents and metadata are properly sorted
 ## note that no covariates are used but content and prevalance terms could be easily added
 t0<-proc.time()
-cat(c(rep('#',10),' Fitting content = ~party ',rep('#',10)),sep='')
+cat(c(rep('#',10),' Fitting content = ~party ',rep('#',10),'\n\n'),sep='')
 if(identical(names(bow2stm$documents),meta$speechID)) {
 	mod<-stm(bow2stm$documents,bow2stm$vocab,K=10,data=meta,content = ~party)
 }
@@ -57,7 +57,7 @@ cat('STM model fit in',round((t1-t0)/60,2)[3],'minutes.')
 save(mod,file='stm-model-cont-party.RData')
 
 t0<-proc.time()
-cat(c(rep('#',10),' Fitting prevalence = ~after911*party ',rep('#',10)),sep='')
+cat(c(rep('#',10),' Fitting prevalence = ~after911*party ',rep('#',10),'\n\n'),sep='')
 if(identical(names(bow2stm$documents),meta$speechID)) {
 	mod<-stm(bow2stm$documents,bow2stm$vocab,K=10,data=meta,prevalence = ~after911*party)
 }
